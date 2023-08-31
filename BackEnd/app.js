7aeac868c708a1af22cc41a878212862cf8342b8
emmanuel munezero <emmanuelmunezero@gmail.com>
2023-08-31 21:44:24 +0200

    remove conflict

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,9 +1,22 @@
 const express=require('express');
 const app=express();
+ const bodyParser=require('body-parser');
+ const mysql=require('mysql');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 
 app.get('/',(req,res)=>{
     res.send('welcome to the server');
+})
+
+const pool=mysql.createPool({
+    connectionLimit:10,
+    host            :'localhost',
+    user            :'root',
+    password        :"",
+    database        :"mybusiness"
 })
 
 
