2016021f6f48b1733a90c942fa1a281c27319dc9
emmanuel munezero <emmanuelmunezero@gmail.com>
2023-10-20 18:42:31 +0200

    user Authentication User Registration and Login

--- BackEnd/Models/userSchema.js
+++ BackEnd/Models/userSchema.js
@@ -4,9 +4,14 @@ const userSchema=new mongoose.Schema({
     lastName:{type:String,required:true},
     email:{type:String,required:true,unique:true},
     telephone:{type:String,required:true,unique:true,max:10},
+    password:{type:String,required:true,min:6},
     businessName:{type:String,required:true,unique:true},
     businessAddress:{type:String,required:true},
+    businessCategory:{type:String,default:'Shop'},
+    Created_at: { type: Date, default: Date.now },
+
 });
+
 
 const Users=mongoose.model('Users',userSchema);
 module.exports=Users;
