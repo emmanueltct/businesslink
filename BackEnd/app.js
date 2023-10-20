91edd6bc0c123d944add9386a986fa8e274ac7d3
emmanuel munezero <emmanuelmunezero@gmail.com>
2023-10-20 12:58:14 +0200

    MERN authentication creating Users Schema

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,11 +1,13 @@
 const express= require('express');
 const mongoose=require('mongoose');
 const app=express();
- const bodyParser=require('body-parser');
-
+const bodyParser=require('body-parser');
+const Users=require("./Models/userSchema");
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+//mongo database connection from localhost
 
 const url="mongodb://127.0.0.1:27017/businessLink";
 
